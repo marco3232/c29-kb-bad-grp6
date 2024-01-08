30f690f93d8312ca661157e39d6daed44582d10b
atehtchan <thetac@gmail.com>
2024-01-08 17:35:47 +0800

    update DB setting

--- knexfile.ts
+++ knexfile.ts
@@ -1,40 +1,22 @@
 import type { Knex } from 'knex'
 import { env } from './env'
 
+let pgConfig: Knex.Config = {
+  client: 'pg', 
+    connection: {
+      database: env.DB_NAME,
+      user: env.DB_USERNAME,
+      password: env.DB_PASSWORD,
+      host: env.DB_HOST,
+      port: env.DB_PORT,
+      multipleStatements: true,
+    },
+}
+
 const config: { [key: string]: Knex.Config } = {
-  development: {
-    client: 'pg',
-    connection: {
-      database: env.DB_NAME,
-      user: env.DB_USERNAME,
-      password: env.DB_PASSWORD,
-      host: env.DB_HOST,
-      port: env.DB_PORT,
-      multipleStatements: true,
-    },
-  },
-  test: {
-    client: 'pg',
-    connection: {
-      database: env.DB_NAME,
-      user: env.DB_USERNAME,
-      password: env.DB_PASSWORD,
-      host: env.DB_HOST,
-      port: env.DB_PORT,
-      multipleStatements: true,
-    },
-  },
-  production: {
-    client: 'pg',
-    connection: {
-      database: env.DB_NAME,
-      user: env.DB_USERNAME,
-      password: env.DB_PASSWORD,
-      host: env.DB_HOST,
-      port: env.DB_PORT,
-      multipleStatements: true,
-    },
-  }
+  development: pgConfig,
+  test: pgConfig,
+  production: pgConfig,
 }
 
 module.exports = config;
