d1a11b8ee39edd2e65a351c5c536a537b39a6e0c
atehtchan <thetac@gmail.com>
2024-01-08 18:17:28 +0800

    knex

--- db.ts
+++ db.ts
@@ -1,12 +1,12 @@
-import Knex from 'knex'
-import { env } from './env'
+import Knex from "knex";
+import { env } from "./env";
 
 export function createKnex() {
-    let config = require('./knexfile')
-    let profile = config[env.NODE_ENV]
-    
-    let knex = Knex(profile)
-    return knex
+  let config = require("./knexfile");
+  let profile = config[env.NODE_ENV];
 
+  let knex = Knex(profile);
+  return knex;
 }
 
+export const knex = createKnex();
