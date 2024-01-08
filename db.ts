e310712f3dc76bf7d6debe309b4fbad389b489cf
atehtchan <thetac@gmail.com>
2024-01-08 15:48:28 +0800

    create knex.test.ts

--- db.ts
+++ db.ts
@@ -1,7 +1,12 @@
 import Knex from 'knex'
 import { env } from './env'
 
-let config = require('./knexfile')
-let profile = config[env.NODE_ENV]
+export function createKnex() {
+    let config = require('./knexfile')
+    let profile = config[env.NODE_ENV]
+    
+    let knex = Knex(profile)
+    return knex
 
-export let knex = Knex(profile)
+}
+
