0fdae775af90185a8bcb2a2767dca71b651001aa
marco.wong <s1993618@yahoo.com.hk>
2024-01-11 17:30:50 +0800

    post tripplan to json

---
+++ public/js/tripplan.js
@@ -0,0 +1,31 @@
+console.log("tripplan")
+
+document
+  .querySelector("#tripplan")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+    console.log("testtt")
+
+    const form = event.target
+    const formObject = {
+      numberOfRenters: form.numberOfRenters.value,
+      relationship: form.relationship.value,
+      ageRange: form.ageRange.value,
+      rentalDays: form.rentalDays.value,
+      rentalPurpose: form.rentalPurpose.value,
+    }
+    console.log("formObject",formObject)
+    const res = await fetch('/tripplan', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formObject),
+
+    })
+    console.log("RES",res)
+    // const result = await res.json()
+    // document.querySelector('#contact-result').textContent = result.error
+  });
+
+  
