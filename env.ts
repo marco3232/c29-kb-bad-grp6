52fa878423e4220b5be78774a23403779ecef516
atehtchan <thetac@gmail.com>
2024-01-11 11:13:35 +0800

    wait for merge

--- env.ts
+++ env.ts
@@ -21,7 +21,7 @@ function loadEnv() {
           TEST_DB_NAME: '',
           TEST_DB_USERNAME: '',
           TEST_DB_PASSWORD: '',
-          SESSION_SECRET: 'jodsjgklsgd',
+          SESSION_SECRET: '',
         }
       populateEnv(env, { mode: 'halt' })
       return {
@@ -42,7 +42,7 @@ function loadEnv() {
           POSTGRES_PASSWORD: '',
           POSTGRES_HOST: '',
           POSTGRES_PORT: 5432,
-          SESSION_SECRET: 'jodsjgklsgd',
+          SESSION_SECRET: '',
         }
       populateEnv(env, { mode: 'halt' })
       // console.log("check2",env)
@@ -65,7 +65,7 @@ function loadEnv() {
           DB_NAME: '',
           DB_USERNAME: '',
           DB_PASSWORD: '',
-          SESSION_SECRET: 'jodsjgklsgd',
+          SESSION_SECRET: '',
         }
       populateEnv(env, { mode: 'halt' })
       // console.log("check env",env)
