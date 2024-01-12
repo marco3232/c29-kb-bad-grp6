22a92d0ffe1acdb1db22c3c25f9fb8cfce92a96d
marco.wong <s1993618@yahoo.com.hk>
2024-01-12 17:10:05 +0800

    waiting to merge

--- public/python/pyserver.py
+++ public/python/pyserver.py
@@ -1,19 +1,14 @@
 from flask import Flask, request, jsonify
-# from flask_cors import CORS
+
 
 app = Flask(__name__)
-# CORS(app)
 
-# @app.route('/tripplan')
-# def index():
-#     return "Flask server"
 
 @app.route('/tripplan', methods = ['POST'])
 def postdata():
-    data = request.get_json()
-    print(data)
-# do something with this data variable that contains the data from the node server
-    # return json.dumps({"newdata":"hereisthenewdatayouwanttosend"})
+    # data = request.get_json()
+    # print(data)
+
     return jsonify({ "result" : data})
 
 
