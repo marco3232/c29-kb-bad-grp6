1f3e14660eda8eb5746d0caa6fca4628153a8c7b
atehtchan <thetac@gmail.com>
2024-01-15 17:05:39 +0800

    update loading

--- public/js/tripplan.js
+++ public/js/tripplan.js
@@ -28,8 +28,32 @@ document
     console.log("im frontend",res)
     // const result = await res.json()
     // document.querySelector('#contact-result').textContent = result.error
-    
+
+    // document.querySelector('loadingGif').style.display = 'block';
 
   });
 
-  
+
+async function loading() {
+  document.querySelector("#loading").style.display = "block";
+  console.log("now loading")
+    
+  try {
+    // Perform a fetch request (replace 'url' with your actual URL)
+    await fetch('/tripplan_result');
+
+    // Delay the page navigation for 10 seconds
+    setTimeout(() => {
+        // Navigate to another page (replace 'newPage.html' with your actual page)
+        window.location.href = 'tripplan_result.html';
+    }, 10000); // 10000 milliseconds = 10 seconds
+} catch (error) {
+    // Handle any fetch errors here
+    console.error('Error during fetch:', error);
+}
+
+// To prevent the form from submitting, return false
+return false;
+}
+
+
